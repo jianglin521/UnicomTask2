ddcec80c120fe3f314818ef2b8390fc13e213177
zhuangzhuang <17610835880@163.com>
2021-09-12 10:13:53 +0800

    修改

--- checkin_sports.py
+++ checkin_sports.py
@@ -114,8 +114,8 @@ def get_app_token(login_token):
     url = f"https://account-cn.huami.com/v1/client/app_tokens?app_name=com.xiaomi.hm.health&dn=api-user.huami.com%2Capi-mifit.huami.com%2Capp-analytics.huami.com&login_token={login_token}"
     response = requests.get(url,headers=headers).json()
     app_token = response['token_info']['app_token']
-    #print("app_token获取成功！")
-    #print(app_token)
+    print("app_token获取成功！")
+    print(app_token)
     return app_token
     
 if __name__ ==  "__main__":
